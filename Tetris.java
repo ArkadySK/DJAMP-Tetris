958bde2d51f11f6b9b38c36b17d43224a20f002d
arkadySK <adamkooo2@gmail.com>
2022-11-03 15:41:02 +0100

    pridany .gitignore, vytvoreny projekt

---
+++ Tetris.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Tetris here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tetris {
+    /**
+     * Constructor for objects of class Tetris
+     */
+    public Tetris() {
+        // initialise instance variables
+    }
+}
