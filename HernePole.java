c37253e5e3a675b8b890b6a95c66e0dd107ed795
arkadySK <adamkooo2@gmail.com>
2022-11-10 16:21:57 +0100

    vytvorene triedy Hra, HernePole

---
+++ HernePole.java
@@ -0,0 +1,16 @@
+public class HernePole {
+
+    private ArrayList<Tvar> HerneObjekty;
+
+    public HernePole()  {
+
+    }
+
+    public void aktualizuj()    {
+
+    }
+
+    public void vytvorTvar()    {
+        
+    }
+}
---
+++ Hra.java
@@ -0,0 +1,11 @@
+public class Hra {
+    private boolean jeKoniec;
+    private hernePole pole;
+
+    public Hra()    {
+
+    }
+
+    
+    
+}
